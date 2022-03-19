fbb49a0370a2ef40efb93562c871395512085c48
tomokiqi <tomokiqi@gmail.com>
2022-03-19 17:22:21 +0900

    create global style file

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Fragment } from "react";
+import { GlobalStyle } from "../globalStyle";
+
+export default function Index() {
+  return (
+    <Fragment>
+      <GlobalStyle />
+    </Fragment>
+  );
+}
